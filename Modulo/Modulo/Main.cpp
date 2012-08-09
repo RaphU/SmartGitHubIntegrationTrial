602a149faec832af15c9a8ee1cd012fc2ab7664d
Raphael Unglik <raphoul@gmail.com>
2012-08-09 13:12:36 +0300

    encore un essai

--- Modulo/Modulo/Main.cpp
+++ Modulo/Modulo/Main.cpp
@@ -1,12 +1,13 @@
 
 #include <iostream>
+#include "Stam.h"
 
 using namespace std;
 
 
 void main()
 {
-	int tmp;
+	unsigned int tmp = stam::stamUInt;
 	cin >> tmp;
 	cout << (tmp / 10) << " " << (tmp % 10) << " " << (tmp - (tmp% 10)) << endl;
 	cin >> tmp;
---
+++ Modulo/Modulo/Stam.h
@@ -0,0 +1,9 @@
+
+#pragma once
+
+class stam
+{
+public:
+	static const unsigned int stamUInt = 0;
+};
+
