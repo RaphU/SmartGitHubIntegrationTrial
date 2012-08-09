74075293414df6cca8e26bce76538f1341793ca2
Raphael Unglik <raphoul@gmail.com>
2012-08-09 12:53:13 +0300

    Initial commit

---
+++ Modulo/Modulo/Main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+void main()
+{
+	int tmp;
+	cin >> tmp;
+	cout << (tmp / 10) << " " << (tmp % 10) << " " << (tmp - (tmp% 10)) << endl;
+	cin >> tmp;
+}
+
